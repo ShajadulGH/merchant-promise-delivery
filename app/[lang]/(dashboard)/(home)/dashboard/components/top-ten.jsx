a1f3d6e53d79cc56dc1440d3cd0a2d8414bde6fe
Shajadul Karim Bhuiyan <shajadulrahe@gmail.com>
2025-02-25 16:07:03 +0600

    feat-add single & bulk delivery quick link

--- app/[lang]/(dashboard)/(home)/dashboard/components/top-ten.jsx
+++ app/[lang]/(dashboard)/(home)/dashboard/components/top-ten.jsx
@@ -7,18 +7,19 @@ import SingleDelivery from "@/public/images/social/single-delivery.png";
 import parcel from "@/public/images/social/parcel.png";
 import Invoices from "@/public/images/social/invoice.png";
 import Issue from "@/public/images/social/issue-history.png";
+import Link from "next/link";
 
 const linkList = [
   {
     id: 1,
     name: "Create Bulk Delivery",
-    link: "www.facebook.com",
+    link: "/bulk-order",
     image: delivery,
   },
   {
     id: 2,
     name: "Create Single Delivery",
-    link: "www.instagram.com",
+    link: "/add-parcel",
     image: SingleDelivery,
   },
   {
@@ -49,31 +50,32 @@ const TopTen = () => {
       <CardContent className="px-0">
         <div>
           {linkList.map((item, index) => (
-            <div
-              className="flex flex-wrap  gap-2 hover:bg-default-50 py-[11px] px-4"
-              key={`social-item-${index}`}
-            >
-              <div className="flex-1 flex items-center gap-3">
-                <div className="w-8 h-8 md:h-10 md:w-10">
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div>
-                  <div className="text-sm font-medium text-default-600">
-                    {item.name}
+            <Link href={item.link}>
+              <div
+                className="flex flex-wrap  gap-2 hover:bg-default-50 py-[11px] px-4"
+                key={`social-item-${index}`}
+              >
+                <div className="flex-1 flex items-center gap-3">
+                  <div className="w-8 h-8 md:h-10 md:w-10">
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      className="w-full h-full object-cover"
+                    />
                   </div>
-                  {/* <div className="text-xs text-default-500"> {item.link}</div> */}
+                  <div>
+                    <div className="text-sm font-medium text-default-600">
+                      {item.name}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center gap-1.5">
+                  <div className="h-5 w-5 rounded-full bg-primary bg-opacity-10  justify-center items-center hidden md:flex">
+                    <ChevronRight className="w-2.5 h-2.5 text-primary" />
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center gap-1.5">
-                <div className="h-5 w-5 rounded-full bg-primary bg-opacity-10  justify-center items-center hidden md:flex">
-                  <ChevronRight className="w-2.5 h-2.5 text-primary" />
-                </div>
-              </div>
-            </div>
+            </Link>
           ))}
         </div>
       </CardContent>
