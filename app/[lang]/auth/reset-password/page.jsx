a5e486348e83b44a6f695f000c15311f0a3d9211
Shajadul Karim Bhuiyan <shajadulrahe@gmail.com>
2025-03-18 11:44:53 +0600

    update-login bg image

--- app/[lang]/auth/reset-password/page.jsx
+++ app/[lang]/auth/reset-password/page.jsx
@@ -1,29 +1,25 @@
 "use client";
 import Image from "next/image";
-import background from "@/public/loginImage.jpg";
+import background from "@/public/login-bg-2.jpg";
 import CreatePassword from "../reset-password/create-password-form";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 const RegisterPage = () => {
+  const smallScreen = useMediaQuery("(max-width: 768px)");
   return (
     <>
-      <div className="min-h-screen bg-background  flex items-center  overflow-hidden w-full">
-        <div className="min-h-screen basis-full flex flex-wrap w-full  justify-center overflow-y-auto">
-          <div
-            className="basis-1/2 bg-primary w-full  relative hidden xl:flex justify-center items-center bg-gradient-to-br
-          from-primary-600 via-primary-400 to-primary-600
-         "
-          >
-            <Image
-              src={background}
-              alt="image"
-              className="absolute top-0 left-0 w-full h-full object-cover "
-              layout="fill"
-            />
-          </div>
-          <div className=" min-h-screen basis-full md:basis-1/2 w-full px-4 py-5 flex justify-center items-center">
-            <div className="lg:w-[480px] ">
-              <CreatePassword />
-            </div>
+      <div className="min-h-screen grid grid-cols-2 items-center overflow-hidden w-full bg-red-100">
+        <div className="col-span-2 lg:col-span-1"></div>
+        {!smallScreen && (
+          <Image
+            src={background}
+            alt="Background Image"
+            className="absolute top-0 left-0 w-full h-full"
+          />
+        )}
+        <div className="col-span-2 lg:col-span-1 flex justify-center">
+          <div className=" w-full bg-background py-5 max-w-xl  rounded-sm relative z-10 2xl:p-16 xl:p-12 p-10 m-4 shadow-lg ">
+            <CreatePassword />
           </div>
         </div>
       </div>
